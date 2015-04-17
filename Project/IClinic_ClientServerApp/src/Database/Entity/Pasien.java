fec6bcc30a50ad9549d6b0e5b97539e491caa6e1
AnangAQoyroni <anang_qoyroni@rocketmail.com>
2015-04-17 11:52:43 +0700

    Database Pasien
    do = menambah variabel tanggal lahir dan tanggal daftar
    menghapus variabel TTL
    to do = gagal daftar karena field kosong

--- Project/IClinic_ClientServerApp/src/Database/Entity/Pasien.java
+++ Project/IClinic_ClientServerApp/src/Database/Entity/Pasien.java
@@ -16,11 +16,12 @@ public class Pasien implements Serializable  {
     private int Id_Pasien;
     private String Nama_Pasien;
     private String Alamat;
-    private Date TTL;
+    private Date Tanggal_Lahir;
     private int Usia;
     private int No_HP;
     private String Jenis_Kelamin;
     private int No_Ktp;
+    private Date Tanggal_Daftar;
     
     public void setNo_Ktp(int No_Ktp){
         this.No_Ktp = No_Ktp;
@@ -41,12 +42,15 @@ public class Pasien implements Serializable  {
         return Alamat;
     }
 
-    public Date getTTL() {
-        return TTL;
+    public Date Tanggal_Lahir() {
+        return Tanggal_Lahir;
     }
 
     public int getUsia() {
         return Usia;
+    }
+    public Date getTanggal_Daftar() {
+        return Tanggal_Daftar;
     }
 
     public int getNo_HP() {
@@ -69,11 +73,14 @@ public class Pasien implements Serializable  {
         this.Alamat = Alamat;
     }
 
-    public void setTTL(Date TTL) {
-        this.TTL = TTL;
+    public void setTanggal_Lahir(Date Tanggal_Lahir) {
+        this.Tanggal_Lahir = Tanggal_Lahir;
     }
 
     public void setUsia(int Usia) {
+        this.Usia = Usia;
+    }
+    public void setTanggal_Daftar(Date Tanggal_Daftar) {
         this.Usia = Usia;
     }
 
