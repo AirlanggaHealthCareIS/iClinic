f6e6d70bfdb7d9a86ba1dce942578cd1eac9075a
Tiara Ratna Sari <tiaratnasari12@gmail.com>
2015-05-09 17:29:33 +0700

    Test Case Berhasil Memproses Pembayaran dgn Debit
    DO : Mengimplementasi JUnit untuk skenario Berhasil Memproses Pembayaran
    Pasien dengan Debit
    
    DO TO: Merevisi implementasi yang masih kurang benar

---
+++ Project/IClinic_ServerApp/test/MemprosesPembayaranPasien_22/BerhasilMemprosesPembayaranPasiendenganDebit.java
@@ -0,0 +1,81 @@
+package MemprosesPembayaranPasien_22;
+
+import Database.Entity.Pembayaran;
+import ServerApplication.Service.Bag_PembayaranServiceServer;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Asus X455L
+ */
+public class BerhasilMemprosesPembayaranPasiendenganDebit implements Remote {
+
+    Bag_PembayaranServiceServer bag_PembayaranServiceServer;
+
+    public BerhasilMemprosesPembayaranPasiendenganDebit() throws RemoteException {
+        this.bag_PembayaranServiceServer = new Bag_PembayaranServiceServer();
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() throws RemoteException {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testUpdateStatusPembayaran() throws Exception {
+        System.out.println("updateStatusPembayaran");
+        
+        bag_PembayaranServiceServer = new Bag_PembayaranServiceServer();
+        String Id_Pembayaran = "BYR0002";
+        String Status = "LUNAS DEBIT";
+        
+        String instance = bag_PembayaranServiceServer.updateStatusPembayaran(Id_Pembayaran, Status);
+        System.out.println(instance);
+        
+        String expResult = "LUNAS DEBIT";
+        
+        assertEquals(expResult, instance);
+    }
+
+    /**
+     * Test of getStatus method, of class Bag_PembayaranServiceServer.
+     */
+    @Test
+    public void testGetStatus() throws Exception {
+        System.out.println("getStatus");
+        
+        bag_PembayaranServiceServer = new Bag_PembayaranServiceServer();
+        String Id_Pembayaran = "BYR0002";
+        
+        String instance = bag_PembayaranServiceServer.getStatus(Id_Pembayaran);
+        System.out.println(instance);
+        
+        String expResult = "LUNAS DEBIT";
+        
+        assertEquals(expResult, instance);
+    }
+}
