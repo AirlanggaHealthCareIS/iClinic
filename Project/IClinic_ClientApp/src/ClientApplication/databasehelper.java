d38bec5ec9414de53b1be0d5eb35a78be7bfa534
Kusumaningtyas Aditya Putri <k.aditya.putri@gmail.com>
2015-06-05 14:13:22 +0700

    Revisi databasehelper

--- Project/IClinic_ClientApp/src/ClientApplication/databasehelper.java
+++ Project/IClinic_ClientApp/src/ClientApplication/databasehelper.java
@@ -5,6 +5,16 @@
 
 package ClientApplication;
 
+import Database.Service.AdministratorService;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,12 +29,34 @@ import java.util.logging.Logger;
  */
 public class databasehelper {
     private static Connection connection;
+    
+    static Registry registry;
+    static AdministratorService service;
+    static FormLogin logForm;
+    static String ipServer = "";
+    static BufferedReader br;
+    static File file = new File("ipserver.txt");
 
-     public static Connection getConnection() {
-       if(connection == null){
+    public static Connection getConnection() throws RemoteException {
+        try {
+            if (file.exists()) {
+                br = new BufferedReader(new FileReader(file));
+                ipServer = br.readLine();
+                registry = LocateRegistry.getRegistry(ipServer, 6789);
+                service = (AdministratorService) registry.lookup("service");
+                logForm = new FormLogin(service, ipServer);
+                logForm.setVisible(true);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch(NotBoundException e){
+            e.printStackTrace();
+        }
+        String connectTo = "jdbc:mysql://" + ipServer + ":3306/klinik2";
+        if(connection == null){
             try {
                 DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/klinik2","root","");
+                connection = DriverManager.getConnection(connectTo,"root","");
             } catch (SQLException ex) {
                 Logger.getLogger(databasehelper.class.getName()).log(Level.SEVERE, null, ex);
             }
